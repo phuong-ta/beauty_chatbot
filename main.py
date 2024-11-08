05e7e3677e59cb48e5a7bdb7e09e9bae3a5ae96c
phuong <phuong.ta011290@gmail.com>
2024-11-08 23:38:54 +0200

    project modifed! and UI added!

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import os
 
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
-
-from routers.chatbot import chatbot_router
-from routers.index import index_router
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-app.include_router(index_router)
-app.include_router(chatbot_router)
-load_dotenv()
-OPENAI_KEY = os.getenv('OPENAI_KEY')
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="index.html", context={"response": "OK La"}
+    )
+
+#load_dotenv()
+#OPENAI_KEY = os.getenv('OPENAI_KEY')
